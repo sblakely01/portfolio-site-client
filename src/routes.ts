e368208220f71aa13b9c09aefeac99f4a9f99226
stephanye <stephanye.blakely@gmail.com>
2022-08-07 22:58:32 -0400

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+// src/routes.ts
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// other
+import {FC} from "react";
+
+// interface
+interface Route {
+    key: string,
+    title: string,
+    path: string,
+    enabled: boolean,
+    component: FC<{}>
+}
+
+export const routes: Array<Route> = [
+    {
+        key: 'home-route',
+        title: 'Home',
+        path: '/',
+        enabled: true,
+        component: Home
+    },
+    {
+        key: 'about-route',
+        title: 'About',
+        path: '/about',
+        enabled: true,
+        component: About
+    }
+]
